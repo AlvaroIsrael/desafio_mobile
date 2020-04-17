51f7a466aaaa268e16578a7e884f4b4a48cebdf3
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-04-16 21:48:14 -0300

    chore: Rebase of the project.

--- App.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import AppContainer from './src/v/JanelaControlador';
-
-export default class App extends Component {
-  render() {
-    return <AppContainer/>;
-  }
-}
--- src/m/Contantes.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const kGitHubSails = 'https://api.github.com/repos/balderdashy/sails/releases';
